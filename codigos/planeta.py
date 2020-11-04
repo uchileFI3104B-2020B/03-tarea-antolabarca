c1d872286b765b80008eb534f7be681d890dc172
antolabarca <anto.labarca.s@gmail.com>
2020-11-04 12:51:57 -0300

    se implementa el metodo de runge kutta

--- codigos/planeta.py
+++ codigos/planeta.py
@@ -29,7 +29,33 @@ class Planeta(object):
         aux = - r**(-3) + 2*self.alpha*r**(-4)
         fx = x * aux
         fy = y * aux
-        return [vx, vy, fx, fy]
+        return np.array([vx, vy, fx, fy])
+
+
+    # los siguientes métodos corresponden al cálculo de Runge Kutta de Orden 4
+    def K1(self, dt):
+        """
+        Calcula la cte K1 del algoritmo de Runge-Kutta
+        """
+        return dt * self.ecuacion_de_movimiento()
+
+    def K2(self, dt):
+        """
+        Calcula la cte K2 del algoritmo de Runge-Kutta
+        """
+        return dt * self.ecuacion_de_movimiento()
+
+    def K3(self, dt):
+        """
+        Calcula la cte K3 del algoritmo de Runge-Kutta
+        """
+        return dt * self.ecuacion_de_movimiento()
+
+    def K4(self, dt):
+        """
+        Calcula la cte K4 del algoritmo de Runge-Kutta
+        """
+        return dt * self.ecuacion_de_movimiento()
 
     def avanza_rk4(self, dt):
         """
@@ -38,7 +64,20 @@ class Planeta(object):
         retorna nada, pero modifica los valores de self.y_actual y de
         self.t_actual.
         """
-        pass
+        k1_n = self.K1(dt)
+        self.t_actual += dt/2
+        self.y_actual += k1_n/2
+        k2_n = self.K2(dt)
+        self.y_actual -= k1_n/2
+        self.y_actual += k2_n/2
+        k3_n = self.K3(dt)
+        self.t_actual += dt/2
+        self.y_actual += k3_n
+        self.y_actual -= k2_n/2
+        k4_n = self.K4(dt)
+        ans = self.y_actual + (k1_n + 2*k2_n + 2*k3_n + k4_n)/6
+        self.y_actual = ans
+
 
     def avanza_verlet(self, dt):
         """
